217d0ef9fced34200458c8a634c1447648241758
Basheer Thiabat <bcheer@yahoo.com>
2022-06-03 06:32:25 +0300

    Update slider-data.js

--- src/slider-data.js
+++ src/slider-data.js
@@ -1,22 +1,15 @@
  const sliderData = [
 
-{
-    img:'./slider-images/slide7.jpeg'
-},
-
+ 
 {
     img:'./slider-images/Europe-England-London-Big-Ben-MH.jpeg'
 },
 {
     img:'./slider-images/hero--europe--florence.jpeg'
-},{
-    img:'./slider-images/italy.webp'
-},{
-    img:'./slider-images/swiotzerlnd.jpeg'
-},{
+} 
+, 
+{
     img:'./slider-images/switzerland-europe.jpeg'
-},{
-    img:'./slider-images/switzerland.jpeg'
 }
 
 
