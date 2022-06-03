fadadd79789468d22d800305d55774b9f69a9eb4
Basheer Thiabat <bcheer@yahoo.com>
2022-06-03 07:35:21 +0300

    Update locationsData.js

--- src/locationsData.js
+++ src/locationsData.js
@@ -1,31 +1,30 @@
  const locationsData =[
      {
-        city:'Italy',
-        img:  '/slider-images/itl.jpeg', '/slider-images/switzerland-europe.jpeg','/slider-images/Europe-England-London-Big-Ben-MH.jpeg' ],
-        about: " 40 million foreign visitors annually "
-
-
-    },
-     {
-        city: 'London',
-        img: ['/slider-images/Europe-England-London-Big-Ben-MH.jpeg','/slider-images/itl.jpeg'],
-        about: "First tourism destinations in europe"
-    },
+         city: 'London',
+         img: ['/slider-images/Europe-England-London-Big-Ben-MH.jpeg','/slider-images/itl.jpeg'],
+         about: "First tourism destinations in europe"
+        },
+        {
+            city: 'Italy',
+            img: ['/slider-images/Europe-England-London-Big-Ben-MH.jpeg','/slider-images/itl.jpeg'],
+            about: "Second Tourisim destination in europe"
+        },
      {
         city: 'Paris',
-        img: [ '/slider-images/itl.jpeg','/slider-images/Europe-England-London-Big-Ben-MH.jpeg' ],
+        img: ['/slider-images/itl.jpeg',
+              '/slider-images/Europe-England-London-Big-Ben-MH.jpeg' ],
         about: "Paris is the second most visited city in Europe after London"
               
      
     }, 
      {
          city: 'Switzerland',
-         img: [ '/slider-images/Europe-England-London-Big-Ben-MH.jpeg','/slider-images/switzerland-europe.jpeg','/slider-images/hero--europe--florence.jpeg'
+         img: ['/slider-images/Europe-England-London-Big-Ben-MH.jpeg','/slider-images/switzerland-europe.jpeg',
+            '/slider-images/hero--europe--florence.jpeg'
                  ],
          about: "Top destination for many tourism"
              
     }
-    
     ]
 
 export default locationsData
