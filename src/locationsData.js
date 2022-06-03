30429d11df8fa40de28c3d4bbfa957cbaa86f592
Basheer Thiabat <bcheer@yahoo.com>
2022-06-03 06:31:16 +0300

    Update locationsData.js

--- src/locationsData.js
+++ src/locationsData.js
@@ -1,30 +1,29 @@
  const locationsData =[
      {
         city:'Italy',
-        img: ['/slider-images/itl.jpeg', '/slider-images/switzerland-europe.jpeg', '/slider-images/swiotzerlnd.jpeg',
-              '/slider-images/italy.webp', '/slider-images/Europe-England-London-Big-Ben-MH.jpeg' ],
+        img:  '/slider-images/itl.jpeg', '/slider-images/switzerland-europe.jpeg',
+               '/slider-images/Europe-England-London-Big-Ben-MH.jpeg' ],
         about: " 40 million foreign visitors annually "
 
 
     },
      {
         city: 'London',
-        img: ['/slider-images/Europe-England-London-Big-Ben-MH.jpeg','/slider-images/switzerland-europe.jpeg',
-            '/slider-images/swiotzerlnd.jpeg','/slider-images/itl.jpeg','/slider-images/italy.webp'],
+        img: ['/slider-images/Europe-England-London-Big-Ben-MH.jpeg','/slider-images/itl.jpeg'],
         about: "First tourism destinations in europe"
     },
      {
         city: 'Paris',
-        img: ['/slider-images/switzerland-europe.jpeg' ,'/slider-images/itl.jpeg',
-              '/slider-images/Europe-England-London-Big-Ben-MH.jpeg', '/slider-images/switzerland-europe.jpeg' ],
+        img: [ '/slider-images/itl.jpeg',
+              '/slider-images/Europe-England-London-Big-Ben-MH.jpeg' ],
         about: "Paris is the second most visited city in Europe after London"
               
      
     }, 
      {
          city: 'Switzerland',
-         img: ['/slider-images/Europe-England-London-Big-Ben-MH.jpeg','/slider-images/switzerland-europe.jpeg',
-         '/slider-images/swiotzerlnd.jpeg', '/slider-images/italy.webp', '/slider-images/hero--europe--florence.jpeg',
+         img: [ '/slider-images/Europe-England-London-Big-Ben-MH.jpeg','/slider-images/switzerland-europe.jpeg',
+            '/slider-images/hero--europe--florence.jpeg'
                  ],
          about: "Top destination for many tourism"
              
