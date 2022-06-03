a18b9429086d477eec8c9e4f2537e9b3bbb1fdcb
Basheer Thiabat <bcheer@yahoo.com>
2022-06-04 02:16:58 +0300

    Update Welcome.js

--- src/Welcome.js
+++ src/Welcome.js
@@ -7,7 +7,7 @@ const Welcome = ()=>{
                 <div className = 'welcome-heading-div'><h1>Holiday</h1></div>
                 <div className = 'welcome-p-div'>
                     <p>Founded in 2005</p>
-             <p> With a strong presence in inbound travel trade </p>
+             <p> With a strong presence in inbound tour trade </p>
             
                  <p>
                  At Holiday, plan your holiday </p>  
