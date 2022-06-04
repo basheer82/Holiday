61bb13c2d82f662f52f2fcfde68f2fe32a14a58e
Basheer Thiabat <bcheer@yahoo.com>
2022-06-04 23:06:59 +0300

    Update locationsData.js

--- src/locationsData.js
+++ src/locationsData.js
@@ -1,27 +1,27 @@
  const locationsData =[
-        {
-         city: 'London',
-         img: ['/slider-images/dalis.jpeg','/slider-images/ballons.jpeg','/slider-images/ballons.jpeg','/slider-images/dalis.jpeg'],
-         about: "First tourism destinations in europe"
-        },
-        {
-            city: 'Italy',
-            img: [  '/slider-images/ballons.jpeg','/slider-images/dalis.jpeg' ,'/slider-images/ballons.jpeg','/slider-images/dalis.jpeg'],
-            about: "Second Tourisim destination in europe"
-        },
-     {
-        city: 'Paris',
-        img: ['/slider-images/ballons.jpeg','/slider-images/dalis.jpeg' , '/slider-images/ballons.jpeg','/slider-images/dalis.jpeg' ],
-        about: "Paris is the second most visited city in Europe after London"
-              
-     
-    }, 
-     {
-         city: 'Switzerland',
-         img: [ '/slider-images/ballons.jpeg','/slider-images/dalis.jpeg',  '/slider-images/ballons.jpeg','/slider-images/dalis.jpeg'],
-         about: "Top destination for many tourism"
+    {
+        city: 'London',
+        img: ['/slider-images/dalis.jpeg' ,'/slider-images/ballons.jpeg','/slider-images/ballons.jpeg','/slider-images/dalis.jpeg'],
+        about: "First tourism destinations in europe"
+       },
+       {
+           city: 'Italy',
+           img: [ '/slider-images/ballons.jpeg','/slider-images/dalis.jpeg','/slider-images/ballons.jpeg','/slider-images/dalis.jpeg'],
+           about: "Second Tourisim destination in europe"
+       },
+    {
+       city: 'Paris',
+       img: ['/slider-images/dalis.jpeg','/slider-images/dalis.jpeg'  ,'/slider-images/ballons.jpeg','/slider-images/dalis.jpeg' ],
+       about: "Paris is the second most visited city in Europe after London"
              
-    }
+    
+   }, 
+    {
+        city: 'Switzerland',
+        img: [ '/slider-images/ballons.jpeg','/slider-images/dalis.jpeg', '/slider-images/ballons.jpeg','/slider-images/dalis.jpeg'],
+        about: "Top destination for many tourism"
+            
+   }
     ]
 
 export default locationsData
