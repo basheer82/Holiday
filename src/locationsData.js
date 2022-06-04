199147993cda75accc3f51c028449ae61012130f
Basheer Thiabat <bcheer@yahoo.com>
2022-06-04 19:50:25 +0300

    Update locationsData.js

--- src/locationsData.js
+++ src/locationsData.js
@@ -1,25 +1,24 @@
  const locationsData =[
      {
          city: 'London',
-         img: ['/slider-images/mountain.jpeg','/slider-images/img.jpeg','/slider-images/itl.jpeg'],
+         img: [ '/slider-images/img.jpeg','/slider-images/itl.jpeg'],
          about: "First tourism destinations in europe"
         },
         {
             city: 'Italy',
-            img: ['/slider-images/mountain.jpeg','/slider-images/img.jpeg','/slider-images/itl.jpeg'],
+            img: [ '/slider-images/img.jpeg','/slider-images/itl.jpeg'],
             about: "Second Tourisim destination in europe"
         },
      {
         city: 'Paris',
-        img: ['/slider-images/itl.jpeg',
-              '/slider-images/mountain.jpeg','/slider-images/img.jpeg' ],
+        img: ['/slider-images/itl.jpeg','/slider-images/img.jpeg' ],
         about: "Paris is the second most visited city in Europe after London"
               
      
     }, 
      {
          city: 'Switzerland',
-         img: ['/slider-images/itl.jpeg','/slider-images/mountain.jpeg','/slider-images/img.jpeg' ],
+         img: ['/slider-images/itl.jpeg','/slider-images/img.jpeg' ],
          about: "Top destination for many tourism"
              
     }
