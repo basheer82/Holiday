80ca2d9c047dc733d9ffeae6961cca27e1f686d5
Basheer Thiabat <bcheer@yahoo.com>
2022-06-04 14:57:17 +0300

    Update slider-data.js

--- src/slider-data.js
+++ src/slider-data.js
@@ -4,10 +4,7 @@
 {
     img:'./slider-images/Europe-England-London-Big-Ben-MH.jpeg'
 },
-{
-    img:'./slider-images/hero--europe--florence.jpeg'
-} 
-, 
+ 
 {
     img:'./slider-images/switzerland-europe.jpeg'
 }
