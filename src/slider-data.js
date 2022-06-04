3f52e337ce4794e0bf3277193d064abda616218b
Basheer Thiabat <bcheer@yahoo.com>
2022-06-04 21:10:52 +0300

    Update slider-data.js

--- src/slider-data.js
+++ src/slider-data.js
@@ -1,19 +1,23 @@
  const sliderData = [
 
- 
-
- 
-{
-    img:'./slider-images/switzerland-europe.jpeg'
-},
   {
     img:'./slider-images/imag.jpeg'
 },
   {
     img:'./slider-images/img.jpeg'
-} 
-
-
+} ,
+  
+{
+    img:'./slider-images/dalis.jpeg'
+}
+,
+  {
+    img:'./slider-images/ballons.jpeg'
+}
+   ,
+{
+    img:'./slider-images/switzerland-europe.jpeg'
+}
 ]
 
 export default sliderData
