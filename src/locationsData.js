79f2624a40d13b924162591d982a55efcbde84ee
Basheer Thiabat <bcheer@yahoo.com>
2022-06-04 21:25:52 +0300

    Update locationsData.js

--- src/locationsData.js
+++ src/locationsData.js
@@ -1,7 +1,7 @@
  const locationsData =[
-     {
+        {
          city: 'London',
-         img: [ '/slider-images/img.jpeg','/slider-images/ballons.jpeg','/slider-images/dalis.jpeg','/slider-images/itl.jpeg''/slider-images/ballons.jpeg','/slider-images/dalis.jpeg'],
+         img: ['/slider-images/dalis.jpeg', '/slider-images/img.jpeg','/slider-images/ballons.jpeg','/slider-images/itl.jpeg','/slider-images/ballons.jpeg','/slider-images/dalis.jpeg'],
          about: "First tourism destinations in europe"
         },
         {
@@ -18,7 +18,7 @@
     }, 
      {
          city: 'Switzerland',
-         img: ['/slider-images/ballons.jpeg','/slider-images/dalis.jpeg','/slider-images/itl.jpeg','/slider-images/img.jpeg' '/slider-images/ballons.jpeg','/slider-images/dalis.jpeg'],
+         img: ['/slider-images/itl.jpeg','/slider-images/ballons.jpeg','/slider-images/dalis.jpeg','/slider-images/img.jpeg', '/slider-images/ballons.jpeg','/slider-images/dalis.jpeg'],
          about: "Top destination for many tourism"
              
     }
