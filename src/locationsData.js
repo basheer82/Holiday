2aa7273b0ddfefbd76bfd2d164208b689ee71a5f
Basheer Thiabat <bcheer@yahoo.com>
2022-06-04 22:54:41 +0300

    Update locationsData.js

--- src/locationsData.js
+++ src/locationsData.js
@@ -1,24 +1,24 @@
  const locationsData =[
         {
          city: 'London',
-         img: ['/slider-images/dalis.jpeg', '/slider-images/img.jpeg','/slider-images/ballons.jpeg','/slider-images/itl.jpeg','/slider-images/ballons.jpeg','/slider-images/dalis.jpeg'],
+         img: ['/slider-images/dalis.jpeg','/slider-images/ballons.jpeg','/slider-images/ballons.jpeg','/slider-images/dalis.jpeg'],
          about: "First tourism destinations in europe"
         },
         {
             city: 'Italy',
-            img: [ '/slider-images/itl.jpeg','/slider-images/ballons.jpeg','/slider-images/dalis.jpeg','/slider-images/itl.jpeg','/slider-images/ballons.jpeg','/slider-images/dalis.jpeg'],
+            img: [  '/slider-images/ballons.jpeg','/slider-images/dalis.jpeg' ,'/slider-images/ballons.jpeg','/slider-images/dalis.jpeg'],
             about: "Second Tourisim destination in europe"
         },
      {
         city: 'Paris',
-        img: ['/slider-images/ballons.jpeg','/slider-images/dalis.jpeg','/slider-images/itl.jpeg','/slider-images/img.jpeg','/slider-images/ballons.jpeg','/slider-images/dalis.jpeg' ],
+        img: ['/slider-images/ballons.jpeg','/slider-images/dalis.jpeg' , '/slider-images/ballons.jpeg','/slider-images/dalis.jpeg' ],
         about: "Paris is the second most visited city in Europe after London"
               
      
     }, 
      {
          city: 'Switzerland',
-         img: ['/slider-images/img.jpeg','/slider-images/ballons.jpeg','/slider-images/dalis.jpeg','/slider-images/img.jpeg', '/slider-images/ballons.jpeg','/slider-images/dalis.jpeg'],
+         img: [ '/slider-images/ballons.jpeg','/slider-images/dalis.jpeg',  '/slider-images/ballons.jpeg','/slider-images/dalis.jpeg'],
          about: "Top destination for many tourism"
              
     }
